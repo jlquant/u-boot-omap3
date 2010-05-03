60bc103650ed1084557456ca5430e330e6c65cf9
Sriramakrishnan <srk@ti.com>
2010-05-03 17:05:06 +0530

    TI816x : Increase bootargs string length.
    Increase the bootargs buffer size to 512 bytes.
    Also increase the number of args to 32.
    
    Signed-off-by: Sriramakrishnan <srk@ti.com>

--- include/configs/ti816x_evm.h
+++ include/configs/ti816x_evm.h
@@ -168,12 +168,12 @@ lpj=50000 mem=248M earlyprintk"
 /* Monitor Command Prompt */
 #define CONFIG_SYS_PROMPT		"=> "
 /* Console I/O Buffer Size */
-#define CONFIG_SYS_CBSIZE		256
+#define CONFIG_SYS_CBSIZE		512
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE \
 					+ sizeof(CONFIG_SYS_PROMPT) + 16)
 /* max number of command args */
-#define CONFIG_SYS_MAXARGS		16
+#define CONFIG_SYS_MAXARGS		32
 /* Boot Argument Buffer Size */
 #define CONFIG_SYS_BARGSIZE		CONFIG_SYS_CBSIZE
 
