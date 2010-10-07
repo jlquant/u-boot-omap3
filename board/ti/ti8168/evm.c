bc242e1e0e838ac8c3d2dea7463421861360647f
Madhvapathi Sriram <sriram.m@ti.com>
2010-10-07 17:28:35 +0530

    ti8148 : emac: support for reading mac address for EFUSE
     Read MAC address from EFUSE registers
     If valid set the same mac address
     If not valid get mac address from the env variable "ethaddr"
    
    Signed-off-by: Madhvapathi Sriram <sriram.m@ti.com>

--- board/ti/ti8168/evm.c
+++ board/ti/ti8168/evm.c
@@ -177,9 +177,41 @@ int board_init(void)
 }
 
 #ifdef CONFIG_DRIVER_TI_EMAC
+extern void davinci_eth_set_mac_addr(const u_int8_t *addr)
 int board_eth_init(bd_t *bis)
 {
 	/* TODO : read MAC address from EFUSE */
+        u_int8_t mac_addr[6];
+        u_int8_t mac_addr[6];
+        u_int32_t mac_hi,mac_lo;
+
+        /* try reading mac address from efuse */
+        mac_lo =        __raw_readl(MAC_ID0_LO);
+        mac_hi =        __raw_readl(MAC_ID0_HI);
+        mac_addr[0] = mac_hi & 0xFF;
+        mac_addr[1] = (mac_hi & 0xFF00) >> 8;
+        mac_addr[2] = (mac_hi & 0xFF0000) >> 16;
+        mac_addr[3] = (mac_hi & 0xFF000000) >> 24;
+        mac_addr[4] = mac_lo & 0xFF;
+        mac_addr[5] = (mac_lo & 0xFF) >> 8;
+
+        if(is_valid_ether_addr(mac_addr)) {
+                printf("Detected MACID:%x:%x:%x:%x:%x:%x\n",mac_addr[0],
+                        mac_addr[1], mac_addr[2], mac_addr[3],
+                        mac_addr[4], mac_addr[5]);
+                davinci_eth_set_mac_addr(mac_addr);
+        } else {
+                printf("Invalid MACID, querying environment variable\n");
+                if (!eth_getenv_enetaddr("ethaddr", mac_addr)) {
+                        printf("ENV: ethaddr not set using static MACID\n");
+                } else {
+                        printf("Configure MACID to ethaddr:%x:%x:%x:%x:%x:%x",
+                                mac_addr[0], mac_addr[1], mac_addr[2],
+                                mac_addr[3], mac_addr[4], mac_addr[5]);
+                        davinci_eth_set_mac_addr(mac_addr);
+	}
+	}
+
 	davinci_emac_initialize();
 	return 0;
 }
