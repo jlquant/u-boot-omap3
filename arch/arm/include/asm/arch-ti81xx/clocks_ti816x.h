jlquant/u-boot-omap3

commit 7285eeefaa5a63eb6c6d515dd2bbc0b7394bd9d6

TI816X: updated the MAIN PLL N values for ti8168
This patch updates the MAIN PLL N divider values to
fix the pll locking issue on found during the silicon
characterization.

Signed-off-by: Raj, Deepu <deepu.raj@ti.com>

arch/arm/include/asm/arch-ti81xx/clocks_ti816x.h
@@ -34,29 +34,34 @@
 #define DDR_PLL_796	/* Values supported 400,531,675,796 */
 
 /* Main PLL */
-#define MAIN_N			64
+#define MAIN_N			56
 #define MAIN_P			0x1
-#define MAIN_INTFREQ1		0x8
-#define MAIN_FRACFREQ1		0x800000
-#define MAIN_MDIV1		0x2
+#define MAIN_INTFREQ1		0xE
+#define MAIN_FRACFREQ1		0xE00000
+#define MAIN_MDIV1		0x1
+
 #ifdef ARM_PLL_987
-#define MAIN_INTFREQ2		0xE
-#define MAIN_FRACFREQ2		0x0
+#define MAIN_INTFREQ2		0xC
+#define MAIN_FRACFREQ2		0x400000
 #endif
 #ifdef ARM_PLL_1200
-#define MAIN_INTFREQ2		0xB
-#define MAIN_FRACFREQ2		0x851EB8
+#define MAIN_INTFREQ2		0xA
+#define MAIN_FRACFREQ2		0x147AE1
 #endif
 #define MAIN_MDIV2		0x1
-#define MAIN_INTFREQ3		0x8
-#define MAIN_FRACFREQ3		0xAAAAB0
-#define MAIN_MDIV3		0x3
-#define MAIN_INTFREQ4		0x9
-#define MAIN_FRACFREQ4		0x55554F
-#define MAIN_MDIV4		0x3
-#define MAIN_INTFREQ5		0x9
-#define MAIN_FRACFREQ5		0x374BC6
-#define MAIN_MDIV5		0xC
+
+#define MAIN_INTFREQ3		0xB
+#define MAIN_FRACFREQ3		0x600007
+#define MAIN_MDIV3		0x2
+
+#define MAIN_INTFREQ4		0xC
+#define MAIN_FRACFREQ4		0x3FFFF8
+#define MAIN_MDIV4		0x2
+
+#define MAIN_INTFREQ5		0xC
+#define MAIN_FRACFREQ5		0x189374
+#define MAIN_MDIV5		0x8
+
 #define MAIN_MDIV6		0x48
 #define MAIN_MDIV7		0x4
 

commit 15792a61a67d15d2ac77c03b4f8033cb754ece5e

TI816X: Fixed the USB and Audio PLL divider bug
This patch fixes the USB PHY clk div and AudioPLL
div value bug after changing the MAIN PLL N to 56
introduced by the commit: 7285eee

Signed-off-by: Raj, Deepu <deepu.raj@ti.com>

arch/arm/include/asm/arch-ti81xx/clocks_ti816x.h
@@ -62,7 +62,7 @@
 #define MAIN_FRACFREQ5		0x189374
 #define MAIN_MDIV5		0x8
 
-#define MAIN_MDIV6		0x48
+#define MAIN_MDIV6		0x3F
 #define MAIN_MDIV7		0x4
 
 /* DDR PLL */
@@ -158,17 +158,17 @@
 /* Audio PLL */
 #define AUDIO_N			64
 #define AUDIO_P			0x19
-#define AUDIO_INTFREQ2		0xE
-#define AUDIO_FRACFREQ2		0x0
+#define AUDIO_INTFREQ2		0xC
+#define AUDIO_FRACFREQ2		0x189374
 #define AUDIO_MDIV2		0x4
 #define AUDIO_INTFREQ3		0x9
-#define AUDIO_FRACFREQ3		0x0
-#define AUDIO_MDIV3		0x5
-#define AUDIO_INTFREQ4		0x9
-#define AUDIO_FRACFREQ4		0xCBC148
+#define AUDIO_FRACFREQ3		0xDFD130
+#define AUDIO_MDIV3		0x4
+#define AUDIO_INTFREQ4		0x8
+#define AUDIO_FRACFREQ4		0x92491F
 #define AUDIO_MDIV4		0x14
-#define AUDIO_INTFREQ5		0xD
-#define AUDIO_FRACFREQ5		0x800000
+#define AUDIO_INTFREQ5		0xB
+#define AUDIO_FRACFREQ5		0x0
 #define AUDIO_MDIV5		0x14
 
 #endif	/* endif _CLOCKS_TI816X_H_ */